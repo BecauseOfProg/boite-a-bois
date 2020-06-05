577bae3961f496c0ba9c55a7f36b3dfb3bd6c88d
Théo Vidal <exybore@becauseofprog.fr>
2020-06-05 19:44:57 +0200

    style: improve about display and add license notice

--- src/commands/about.rb
+++ src/commands/about.rb
@@ -5,18 +5,21 @@ module BoiteABois
     class About < Command
       CATEGORY = 'utilities'
       USAGE = 'about'
-      DESC = 'Avoir des informations sur le robot'
+      DESC = 'Avoir des informations pratiques sur Boîte à Bois'
 
       def self.exec(_args, context)
         embed = BoiteABois::Utils::embed(
           title: 'À propos',
           thumbnail: Discordrb::Webhooks::EmbedThumbnail.new(url: $config['illustration']),
-          description: "Boîte à bois est un robot Discord créé par la BecauseOfProg dans le but d'avoir de nombreux utilitaires, mais surtout des jeux.
-Son code source est ouvert à tous : n'hésitez-pas à contribuer à son développement !
+          description:
+            "Boîte à bois est un robot Discord créé par la BecauseOfProg dans le but d'avoir de nombreux utilitaires, mais surtout des jeux.\n" +
+            "Son code source est ouvert à tous : n'hésitez-pas à contribuer à son développement !\n\n" +
 
-🔨 Version : #{$config['version']}
-🌐 Site Internet : #{$config['website']}
-💻 Mainteneur : #{$config['maintainer']['name']} (#{$config['maintainer']['link']})"
+            ":hammer: Version : #{$config['version']}\n" +
+            ":globe_with_meridians: Site Internet : #{$config['website']}\n" +
+            ":computer: Mainteneur : #{$config['maintainer']['name']} (#{$config['maintainer']['link']})\n\n" +
+
+            ":copyright: 2020, BecauseOfProg. Sous licence GNU GPL v3"
         )
         context.send_embed('', embed)
       end
